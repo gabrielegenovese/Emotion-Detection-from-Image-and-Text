5ac8bfde60a52dd8dfa832b2dd1b9c3846f4db9b
geno <gabriele.genovese2@studio.unibo.it>
2024-01-17 18:29:46 +0100

    feat: dataset merged created
    Co-authored-by: kocierik <kocierik@gmail.com>

---
+++ src/scripts/merge_dataset.py
@@ -0,0 +1,45 @@
+import os
+import csv
+import random
+
+def get_images_name(folder):
+    try:
+        if not os.path.exists(folder):
+            raise FileNotFoundError(f"Folder '{folder}' does not exist.")
+
+        file_names = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+        file_names = list(map(lambda x: folder[6:]+"/"+x, file_names))
+        
+        return file_names
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+def create_dataset_from_emotion(csvfile, emotion):
+    filenames = get_images_name(f"../../data/raw/emotion_facial_images/train/{emotion}")
+    random.shuffle(filenames)
+    res = []
+    counter = 0
+    for s in csvfile:
+        if s[1] == emotion:
+           res.append([s[0], filenames[counter], emotion])
+           counter += 1 
+        if counter >= len(filenames):
+            break
+    return res
+
+
+with open('../../data/raw/train_ekmann.csv', newline='') as train_ekmann_f:
+    train_ekmann_csv = csv.reader(train_ekmann_f, delimiter=',', quotechar='"')
+
+    final_list = []
+    dir_list = os.listdir("../../data/raw/emotion_facial_images/train")
+    for dir in dir_list:
+        final_list += create_dataset_from_emotion(train_ekmann_csv, dir)
+        
+    with open('../../data/processed/text_image_emotion.csv', 'w', encoding='UTF8') as newfile_csv:
+        spamwriter = csv.writer(newfile_csv, delimiter=',')
+        for line in final_list:
+            spamwriter.writerow(line)
+
+    
+    
