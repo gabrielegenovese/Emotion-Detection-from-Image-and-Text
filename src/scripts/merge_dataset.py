195448eb6b6c1ac279fdc2668c00e26f20c7f814
geno <gabriele.genovese2@studio.unibo.it>
2024-02-02 17:36:43 +0100

    fix: dataset now is complete

--- src/scripts/merge_dataset.py
+++ src/scripts/merge_dataset.py
@@ -51,11 +51,14 @@ def create_dataset_from_emotion(csvfile, emotion):
 
 with open(trainpath, newline="") as train_ekmann_f:
     train_ekmann_csv = csv.reader(train_ekmann_f, delimiter=",", quotechar='"')
+    csvfile = []
+    for s in train_ekmann_csv:
+        csvfile.append(s)
 
     final_list = []
     dir_list = os.listdir(imagepath)
-    for dir in dir_list:
-        final_list += create_dataset_from_emotion(train_ekmann_csv, dir)
+    for emotion_dir in dir_list:
+        final_list += create_dataset_from_emotion(csvfile, emotion_dir)
 
     with open(newfilepath, "w", encoding="UTF8") as newfile_csv:
         spamwriter = csv.writer(newfile_csv, delimiter=",")
